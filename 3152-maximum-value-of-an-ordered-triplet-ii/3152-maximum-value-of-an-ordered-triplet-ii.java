6ec14d5898433679621f49da8427ec713ee62408
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2025-04-03 18:42:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long answer = 0;
+
+        int iMax = 0; //Max value of nums[i]
+        int ijMax = 0; // Max value of (nums[i] - nums[j])
+
+        for (int num : nums) {
+            answer = Math.max(ijMax * num, answer);
+
+            ijMax = Math.max(iMax - num, ijMax);
+
+            iMax = Math.max(num, iMax);
+        }
+
+        return answer;
+    }
+}
