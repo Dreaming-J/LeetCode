53c62f1b2785932ca370c562d65fe8e9053ed4b3
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2025-04-10 02:19:03 +0900

    Time: 2 ms (82.71%), Space: 44.3 MB (73.22%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,19 +1,23 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        List<Integer> list = Arrays.stream(nums)
-            .boxed()
-            .distinct()
-            .sorted(Collections.reverseOrder())
-            .toList();
+        Long bit1 = 0L;
+        Long bit2 = 0L;
 
-        int listSize = list.size();
-        int minNum = list.get(listSize - 1);
+        for (int num : nums) {
+            if (num > 50)
+                bit2 |= 1L << (num - 50);
+            else
+                bit1 |= 1L << num;
+        }
+
+        int size = Long.bitCount(bit1) + Long.bitCount(bit2);
+        int minValue = bit1 != 0 ? Long.numberOfTrailingZeros(bit1) : Long.numberOfTrailingZeros(bit2) + 50;
         
-        if (minNum < k)
+        if (minValue < k)
             return -1;
-        else if (minNum > k)
-            return listSize;
+        else if (minValue > k)
+            return size;
         else
-            return listSize - 1;
+            return size - 1;
     }
 }
